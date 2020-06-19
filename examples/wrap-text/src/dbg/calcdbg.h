03cff0e09c90d253bd8ea2cd8900e7a8e4748c4f
Captain-Calc <Captain-Calc@users.noreply.github.com>
2020-06-19 14:02:18 -0600

    Update wrap-text example

--- examples/wrap-text/src/dbg/calcdbg.h
+++
@@ -1,7 +0,0 @@
-#ifndef CALCDBG_H
-#define CALCDBG_H
-
-
-void calcdbg_dump(const char *data_buffer, const int length);
-
-#endif
--- examples/wrap-text/src/main.c
+++ examples/wrap-text/src/main.c
@@ -1,64 +1,40 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <tice.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
 #include <graphx.h>
-#include <debug.h>
-#include "dbg/calcdbg.h"
-
-#include <fileioc.h>
 #include <fontlibc.h>
 #include "fonts/fonts.h"
-
 #include <textioc.h>
-
-
-void textio_SetWindow_Outline(unsigned int x, uint8_t y, unsigned int width, uint8_t height) {
-	
-	gfx_SetColor(224);
-	fontlib_SetWindow(x, y, width, height);
-	gfx_Rectangle_NoClip(x, y, width + 1, height);
-}
-
 
 void main(void) {
 	
-    char text[] = {"Welcome to Cemetech! Since 2000, Cemetech (pronounced KE'me'tek) has been teaching programming and electronics and developing software and hardware. Among Cemetech's specialties are TI, HP, and Casio graphing calculators like the TI-84 Plus, TI-Nspire, HP Prime, and Casio Prizm, embedded and DIY electronics, and computer and web programming. Cemetech provides a safe, friendly space for people to learn, show off projects, and share knowledge and expertise. Our staff of friendly volunteers hang out on our forum and IRC and SAX chatrooms, and are happy to help."};
-	char *next_line;
+    char text[] = {"\tThis line starts with a tab.\n\nA demonstration of multiple newline breaks.\n\n\tQuick question: When did indenting paragraphs become a common standard? Did it start with Gutenburg or did someone else invent it in the following centuries after the printing press's invention?\n\t\tDouble tabbing can also help set your text away from the left margin. Tabs in your text while printing centered, however, makes it look off-center."};
 
+	/* Start the graphics */
     gfx_Begin();
-    if (!fontlib_SetFont(test_font, 0))
-		dbg_sprintf(dbgout, "Font failed to load");
-    
+
+	/* Load the font */
+    fontlib_SetFont(test_font, 0);
     fontlib_SetLineSpacing(2, 2);
-    textio_SetWindow_Outline(0, 0, 320, 240);
+	
+	/* Set the text window dimensions*/
+    fontlib_SetWindow(5, 0, 310, 240);
     fontlib_SetColors(0x00, 0xFF);
 	fontlib_HomeUp();
 	
-	dbg_sprintf(dbgout, "width of first line: %d\n", textio_GetLineWidth(text, text + 43));
-	dbg_sprintf(dbgout, "width of first line: %d\n", textio_GetLineWidth(text + 43, text + 85));
-	if (!textio_SetPrintFormat(3)) {
-		dbg_sprintf(dbgout, "Failed to set print format. Terminated program.");
-		goto PRGM_EXIT;
-	};
-	textio_SetTabWidth(10);
+	/* Set print format to left-margin flush */
+	textio_SetPrintFormat(1);
+	
+	/* Set the number of spaces that make up the tab */
+	textio_SetTabSize(5);
+	
+	/* Print the text */
 	textio_PrintText(text, 5);
-	//next_line = test_function(text);
-	//dbg_sprintf(dbgout, "test_function = \'%c\' | 0x%6x\n", *next_line, next_line);
-	dbg_sprintf(dbgout, "line width = %d\n", textio_GetLineWidth(text + 4, text + 9));
-	next_line = textio_GetLinePtr(text, 1);
-	dbg_sprintf(dbgout, "next line starts with \'%c\' | 0x%6x\n", *next_line, next_line);
-	next_line = textio_GetLinePtr(text + 43, 1);
-	dbg_sprintf(dbgout, "next line starts with \'%c\' | 0x%6x\n", *next_line, next_line);
+	
+	/* Outline the window so we can see it */
+	gfx_SetColor(224);
+	gfx_Rectangle_NoClip(5, 0, 310, 240);
+	
 	while (!os_GetCSC());
 	
-	PRGM_EXIT:
+	/* Close the graphics */
     gfx_End();
 	exit(0);
 }
